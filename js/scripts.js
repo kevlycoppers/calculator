cebf795eb71efc744a97ba1d3870161b798811c5
Bo Zhao and Levi Hopkins <levi@>
2016-06-02 14:12:54 -0700

    add radio buttons and easier to use functions

--- js/scripts.js
+++ js/scripts.js
@@ -20,35 +20,24 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (front-end) logic:
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
-		var result = add(number1, number2);
-		$("#output1").text(result);
-	});
-
-	$("form#subtract").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#subtract1").val());
-		var number2 = parseInt($("#subtract2").val());
-		var result = subtract(number1, number2);
-		$("#output2").text(result);
-	});
-
-	$("form#multiply").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#multiply1").val());
-		var number2 = parseInt($("#multiply2").val());
-		var result = multiply(number1, number2);
-		$("#output3").text(result);
-	});
-
-	$("form#divide").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#divide1").val());
-		var number2 = parseInt($("#divide2").val());
-		var result = divide(number1, number2);
-		$("#output4").text(result);
-	});
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+		// console.log("1st number: " + number1);
+		// console.log("2nd number: " + number2);
+		// console.log("operator: " + operator);
+    var result = add(number1, number2);
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract (number1, number2);
+		} else if  (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if  (operator === "divide") {
+			result = divide(number1, number2);
+		}
+    $("#output").text(result);
+  });
 });
